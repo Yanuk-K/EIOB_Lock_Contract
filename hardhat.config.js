872c056f1003af47f9fd3aae44b02ad8c538b2da
Yanuk-K <kimyw102699@gmail.com>
2025-08-15 23:59:02 +0900

    edit ci logic and config to work when env is not present

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,8 +2,9 @@ require("@nomicfoundation/hardhat-ignition-ethers");
 require("dotenv").config();
 require("@nomicfoundation/hardhat-chai-matchers");
 
-const endpointUrl = process.env.ENDPOINT_URL;
-const privateKey = process.env.PRIVATE_KEY;
+const endpointUrl = process.env.ENDPOINT_URL ?? "https://eth-mainnet.g.alchemy.com/public";
+// Placeholder address
+const privateKey = process.env.PRIVATE_KEY ?? "bc476500e724be7203957abddb9c3ff561db9415f29acbacf81b2b1f061da4b3";
 
 module.exports = {
   solidity: "0.8.21",
